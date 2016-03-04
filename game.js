767fafa089df1b1348982354d9245648c456a8aa
Lisa VanOrdenKaan <lmvanorden@gmail.com>
2016-03-03 22:41:19 -0500

    newestupd.html

---
+++ game.js
@@ -0,0 +1,71 @@
+$(document).ready(function() {
+	var randomNum = Math.floor(Math.random() * 120 - 19 + 1) + 19 ;
+	console.log("Random Number = "+randomNum);
+	var userScore = 0;
+	var crystalRunningTot = 0;
+	var blueValue = Math.floor((Math.random() * 12) + 1);
+	console.log("Blue Value = "+blueValue);
+	var redValue = Math.floor((Math.random() * 12) + 1);
+	console.log("Red Value = "+redValue);
+	var greenValue = Math.floor((Math.random() * 12) + 1);
+	console.log("Green Value = "+greenValue);
+	var yellowValue = Math.floor((Math.random() * 12) + 1);
+	console.log("Yellow Value = "+yellowValue);
+});
+	var wins = 0;
+	var losses = 0;
+
+	$(".buttons").on("click", function() {
+	  var currentButton=$(this)[0];
+	  console.log(currentButton.id);
+	});
+
+	  if(currentButton.id == "blue"){
+	    userScore = userScore + blueValue;
+	    console.log("Blue clicked. User Score is now = "+userScore);
+	  }else if(currentButton.id == "red"){
+	    userScore = userScore + redValue;
+	    console.log("Red clicked. User Score is now = "+userScore);
+	  }else if(currentButton.id == "green"){
+	    userScore = userScore + greenValue;
+	    console.log("Green clicked. User Score is now = "+userScore);
+	  }else if(currentButton.id == "yellow"){
+	    userScore = userScore + yellowValue;
+	    console.log("Yellow clicked. User Score is now = "+userScore);
+	  
+
+	  $('#scoreBoard').text("Your total score is: " +userScore);
+
+	  if(userScore == randomNum){
+	    alert("You Win!");
+	    wins++
+	    $('#winBoard').text(wins);
+	    console.log("Wins:   " + wins)
+	    userScore = 0;
+	    blueValue = Math.floor((Math.random() * 12) + 1);
+	    console.log("Blue Value = "+blueValue);
+	    redValue = Math.floor((Math.random() * 12) + 1);
+	    console.log("Red Value = "+redValue);
+	    greenValue = Math.floor((Math.random() * 12) + 1);
+	    console.log("Green Value = "+greenValue);
+	    yellowValue = Math.floor((Math.random() * 12) + 1);
+	    console.log("Yellow Value = "+yellowValue);
+	    randomNum = Math.floor(Math.random() * 120 - 19 + 1) + 19 ;
+	    console.log (randomNum);
+	  }else if(userScore > randomNum) {
+	    alert("You Lose!");
+	    losses++
+	    $('#lossBoard').text(losses);
+	    console.log("Losses:   " + losses);
+	    userScore = 0;
+	    blueValue = Math.floor((Math.random() * 12) + 1);
+	    console.log("Blue Value = "+blueValue);
+	    redValue = Math.floor((Math.random() * 12) + 1);
+	    console.log("Red Value = "+redValue);
+	    greenValue = Math.floor((Math.random() * 12) + 1);
+	    console.log("Green Value = "+greenValue);
+	    yellowValue = Math.floor((Math.random() * 12) + 1);
+	    console.log("Yellow Value = "+yellowValue);
+	    randomNum = Math.floor(Math.random() * 120 - 19 + 1) + 19 ;
+	    console.log (randomNum);
+	   }	 
