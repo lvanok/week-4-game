2d6ca28a3ca8db73c49de76a7456b971bf219fe6
Lisa VanOrdenKaan <lmvanorden@gmail.com>
2016-03-13 18:28:33 -0400

    crystalsgame.html

--- game.js
+++ game.js
@@ -1,11 +1,8 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <meta charset="UTF-8">
-    <title>Inception</title>
-    <script type="text/javascript" src="https://code.jquery.com/jquery-2.2.0.min.js"></script>
 
-<style> 
+<style>
 $(document).ready(function() {
 	var randomNum = Math.floor(Math.random() * 120 - 19 + 1) + 19 ;
 	console.log("Random Number = "+randomNum);
@@ -81,4 +78,3 @@ $(document).ready(function() {
 	</style> 
 	</head>
 	</html>
- 
